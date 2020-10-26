11353807201fb8b5c534a4309b8b0e30979bd0e3
Akash <akash.grep@gmail.com>
2020-10-26 12:54:26 +0530

    Protecting Routes using express-jwt and checking is user authentication and admin

--- Backend/controllers/auth.js
+++ Backend/controllers/auth.js
@@ -65,3 +65,30 @@ exports.signout = (req, res) => {
     message: "User signout"
   });
 };
+
+// protected routes
+exports.isSignedIn = expressJwt({
+  secret: process.env.SECRET,
+  userProperty: "auth"
+});
+
+//Custom Middlewares
+exports.isAuthenticated = (req,res,next) => {
+  let checker = req.profile && req.auth && req.profile._id === req.auth._id;
+
+  if(!checker) {
+    return res.status(403).json({
+      error: "ACCESS_DENIED"
+    })
+  }
+  next();
+}
+
+exports.isAdmin = (req,res,next) => {
+  if(req.profile.role === 0) {
+    return res.status(403).json({
+      error: "YOUR ARE NOT AN ADMIN ACCESS_DENIED"
+    });
+  }
+  next();
+}
