927ab978da9fbe0512101975f6e66c448a512afa
Akash <akash.grep@gmail.com>
2020-10-26 12:55:33 +0530

    Fix typo in authentication route

--- Backend/routes/auth.js
+++ Backend/routes/auth.js
@@ -1,7 +1,7 @@
 var express = require("express");
 var router = express.Router();
 const { check } = require("express-validator");
-const { signout,signin signup } = require("../controllers/auth");
+const { signout,signin, signup } = require("../controllers/auth");
 
 router.post("/signup",[
     check("name","Name should be at least Three characters").isLength({min:3}),
