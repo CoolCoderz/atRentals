01f41bcd0ce00b11265ca8c948f8db501bafda43
Akash <akash.grep@gmail.com>
2020-10-26 12:48:04 +0530

    Added signin and signout routes and controllers

--- Backend/controllers/auth.js
+++ Backend/controllers/auth.js
@@ -23,8 +23,44 @@ exports.signup = (req, res) => {
     });
   });
 };
+exports.signin = (req, res) => {
+  // capturing email and password using destructuring
+  const {email, password} = req.body;
+  //  Validation
+  const errors = validationResult(req)
+  if(!errors.isEmpty()) { 
+    return res.status(422).json({
+      errors: errors.array()[0].msg
+    });
+  }
+  
+  User.findOne({email}, (err, user) => {
+    if(err || !user) {
+      return res.status(400).json({
+        error: "User email does not exist"
+      })
+    }
 
+    if(!user.authenticate(password)) {
+      return res.status(401).json({
+        error: "Email and Password do not match"
+      })
+    }
+// CREATED TOKEN
+    const token =jwt.sign({_id:user._id}, process.env.SECRET);
+
+    // PUT TOKEN IN COOKIE
+    res.cookie("token", token, { expire:new Date() +9999});
+
+    //send response to frontend here we do not want to send all the details of user so 
+    // we are using destructuring
+    const {_id, name, email, role} = user;
+    return res.json({token, user:{_id, name, email, role}});
+  })
+
+}
 exports.signout = (req, res) => {
+  res.clearCookie("token");
   res.json({
     message: "User signout"
   });
