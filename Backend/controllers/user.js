a8e83ebdb87c64d2681b783a51f9a5e36ed3d3d8
Akash <akash.grep@gmail.com>
2020-11-06 00:39:28 +0530

    Written controllers and routes for users

--- Backend/controllers/user.js
+++ Backend/controllers/user.js
@@ -38,3 +38,48 @@ exports.updateUser = (req, res) => {
         }
     )
 }
+
+exports.userPurchaseList = (req, res) => {
+    Order.find({user : req.profile._id})
+    .populate("user","_id name")
+    .exec((err, order)=>{
+        if(err){
+            return res.status(400).json({
+                error : "No Order in this account"
+            });
+        }
+        return res.json(order);
+    });
+}
+
+
+exports.pushOrderInPurchaseList = (req, res, next)=>{
+    
+    let purchases = [];
+    req.body.order.products.forEach(product => {
+        purchases.push({
+            _id: product._id,
+            name: product.name,
+            description: product.description,
+            category: product.category,
+            quantity: product.quantity,
+            amount: req.body.amount,
+            transaction_id: req.body.order.transaction_id,
+        });
+    });
+
+    //Storing in DB
+    User.findOneAndUpdate(
+        {_id:req.profile._id},
+        {$push: {purchases: purchases}},
+        {new: true},
+        (err, purchases) => {
+            if(err) {
+                return res.status(400).json({
+                    error: "Unable to save purchase list",
+                })
+            }
+            next();
+        }
+    )
+}
