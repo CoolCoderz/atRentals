cbaa5446af58bf022012541b0484259fb96f9027
Akash <akash.grep@gmail.com>
2020-11-05 22:52:31 +0530

    Remove a bug in isAuthenticated

--- Backend/controllers/auth.js
+++ Backend/controllers/auth.js
@@ -74,7 +74,7 @@ exports.isSignedIn = expressJwt({
 
 //Custom Middlewares
 exports.isAuthenticated = (req,res,next) => {
-  let checker = req.profile && req.auth && req.profile._id === req.auth._id;
+  let checker = req.profile && req.auth && req.profile._id == req.auth._id;
 
   if(!checker) {
     return res.status(403).json({
