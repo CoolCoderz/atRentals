4cecb4a2b323c6662bd35a60df97ad6b59d767d5
Akash <akash.grep@gmail.com>
2020-12-06 16:59:44 +0530

    modified the code related to express-Validator

--- Backend/routes/auth.js
+++ Backend/routes/auth.js
@@ -1,10 +1,10 @@
 var express = require("express");
 var router = express.Router();
-const { check } = require("express-validator");
+const { check,validationResult } = require("express-validator");
 const { signout,signin, signup } = require("../controllers/auth");
 
 router.post("/signup",[
-    check("name","Name should be at least Three characters").isLength({min:3}),
+    check('name').isLength({min:3}).withMessage("Name should be at least Three characters"),
     check("email","Email is Required").isEmail(),
     check("password","Password should be at least 3 characters").isLength({min:3})
 ], signup);
