683d946dc95739459ed0cb2e0bd37fa4a3411ea8
Mitali-laroia <04042001mitali@gmail.com>
2021-01-26 10:22:55 +0530

    path changes and image type to buffer

---
+++ Backend/models/booksModel.js
@@ -0,0 +1,71 @@
+import mongoose from 'mongoose'
+
+const reviewSchema = mongoose.Schema(
+    {
+        name: {type: String, required:true},
+        author: {type: String, required:true},
+        comment: {type: String,required: false},
+        rating: {type: Number, required: true},
+        user: {
+            type: mongoose.Schema.Types.ObjectId,
+            required:true,
+            ref: 'User',
+        },
+    },
+    {
+        timestamps: true,
+    }
+)
+
+const bookSchema = mongoose.Schema(
+    {
+        user: {
+            type: mongoose.Schema.Types.ObjectId,
+            required: true,
+            ref: 'User',
+        },
+        name :{
+            type: String,
+            required: true,
+        },
+        image: {
+            type: Buffer,
+            required: true,
+        },
+        author:{
+            type: String,
+            required: true,
+        },
+        category:{
+            type: String,
+            required: true,
+        },
+        description:{
+            type: String,
+            required: true,
+        },
+        reviews: [reviewSchema],
+        rating:{
+            type: Number,
+            required: true,
+            default: 0,
+        },
+        price: {
+            type: Number,
+            required: true,
+            default: 0,
+        },
+        countInStock: {
+            type: Number,
+            required: true,
+            default: 0,
+        },
+    },
+    {
+        timestamps: true,
+    }
+)
+
+const Book = mongoose.model('Book', bookSchema)
+
+export default Book
